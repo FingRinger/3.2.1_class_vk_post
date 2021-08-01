b332a15e1a3971fca5417cb47b46677e3102ec41
FingRinger <ard21study@gmail.com>
2021-08-01 12:38:58 +0300

    Initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Post {
+    private String Id;
+    private String titleOfTheMainPageUrl;
+    private String logoUrl;
+    private String dateAndTimeOfPost;
+    private String caption;
+    private String imageUrl;
+    private String buttonLikeUrl;
+    private String buttonCommentsUrl;
+    private String buttonShareUrl;
+    private String outreachButtonUrl;
+    private long outreachOfAudience;
+}
